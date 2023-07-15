ddd50b84b2b551136c8c9051d39474a2c938c253
Karipso <amaral@ucsc.edu>
2023-07-16 00:39:08 +0200

    Adding the mapping as surface reconstruction (tsdf) component to the project. It has a utility class in 'kinect_fusion_utility.h/.cpp' files which include functions such as reading a depth image, finding the pose associated with a depth image etc. (please read the header of the interface). Additionally, it has a modern implementation of a voxel and voxel grid as classes. The projective TSDF and the function to do the global fusion of all depth maps are integrated to the voxel grid class. Please read these classes as the comments are pretty detailed and we can merge these classes with Marc's volume class to use as in our project.

---
+++ src/mapping_as_surface_reconstruction/voxel.cpp
@@ -0,0 +1,7 @@
+#include "voxel.h"
+
+namespace kinect_fusion {
+Voxel() : tsdfValue(std::numeric_limits<double>::quiet_NaN()), 
+          weight(std::numeric_limits<double>::quiet_NaN()), 
+          position(Eigen::Vector3d::Zero()) {}
+}
---
+++ src/mapping_as_surface_reconstruction/voxel.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Eigen/Dense> // for Eigen::Vector3d
+#include <limits>      // for std::numeric_limits
+
+namespace kinect_fusion {
+
+// Voxel class updated to store its position
+class Voxel {
+public:
+  double tsdfValue;  // The TSDF value
+  double weight;  // weight for the distance
+  Eigen::Vector3d position; // The position of the voxel in the global frame
+
+  Voxel();
+};
+
+}
