3503a426275df42d3cde0ca5c8bb95c83a100284
amroabuzer <amroabuzer@gmail.com>
2023-07-15 13:02:40 +0200

    moved private variables down as convention

--- src/icp/GPU_icp.h
+++ src/icp/GPU_icp.h
@@ -1,24 +1,29 @@
 #pragma once
 
 #include <eigen3/Eigen/Dense>
-#include <flann/flann.h>
 #include "../frame/Frame_Pyramid.h"
+#include "NearestNeighbours.h"
 
 
 class ICP{
+
+public:
+
+    ICP(const Frame_Pyramid& F_k, const Frame_Pyramid& F_k__1, const double convergence_threshold): 
+    F_k(F_k), 
+    F_k__1(F_k__1), 
+    convergence_threshold(convergence_threshold){};
+    
+    ~ICP();
+
+    Eigen::Vector4f point_to_plane_solver(Frame & source, Frame & target, int iterations, bool cuda);
+
+    void NN_finder(Eigen::Matrix4f source_transformation, Frame & source, const Frame & target, std::vector<Match>& matches);
+
 private:
 
     const Frame_Pyramid F_k;
     const Frame_Pyramid F_k__1;
     const double convergence_threshold;
-
-public:
-
-    ICP(const Frame_Pyramid& F_k, const Frame_Pyramid& F_k__1, const double convergence_threshold): F_k(F_k), F_k__1(F_k__1), convergence_threshold(convergence_threshold){};
-    ~ICP();
-
-
-    Eigen::Vector4f point_to_plane_solver(Frame & source, Frame & target, int iterations, bool cuda);
-
-    int * NN_finder(Eigen::Vector4f source_transformation, const Frame & source, const Frame & target);
+    NN_flann* my_NN = new NN_flann(1, 0.005f); // should be tweaked later to improve performance
 };
