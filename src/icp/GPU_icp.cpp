accc40a6ba3a1002baad3924e7ad04c6961f1aed
amroabuzer <amroabuzer@gmail.com>
2023-07-18 19:42:28 +0200

    point to plane works now :DDD now making pyramid_to_icp solver

--- src/icp/GPU_icp.cpp
+++ src/icp/GPU_icp.cpp
@@ -12,19 +12,29 @@ void ICP::correspondence_finder(Eigen::Matrix4f T_curr_frame, Frame & curr_frame
     // 5 2 3 1 4 4 prev_frame vector nearest neighbor to source indices
     
     std::vector<Eigen::Vector3f> curr_V_k_transformed; //transformed V
+    std::vector<Eigen::Vector3f> curr_N_k_transformed; //transformed V
 
-    curr_frame.apply_transform(T_curr_frame, curr_V_k_transformed); //transforming current frame's V_k -> V_tk according to previous frame
+    curr_frame.apply_transform(T_curr_frame, curr_V_k_transformed, curr_N_k_transformed); //transforming current frame's V_k -> V_tk according to previous frame
 
-    std::cout << "Finding Correspondences";
+    std::cout << "Finding Correspondences" << std::endl;
 
     for(int i = 0; i < curr_V_k_transformed.size(); i++){
         if(!std::isnan(curr_V_k_transformed[i][0])){
             Eigen::Vector2i pixel = prev_frame.vec_to_pixel(curr_V_k_transformed[i]);
             int idx_in_prev = pixel[1]*prev_frame.width + pixel[0];
             
-            //check if idx_in_prev is a valid vertex in previous frame
-            if(!std::isnan(prev_frame.V_gk[idx_in_prev][0])){
-                matches.push_back(std::make_pair(i, idx_in_prev));
+            //Normals aren't calculated in the last row => check if idx is less than last row idxs
+            if(idx_in_prev < (prev_frame.height-1) * prev_frame.width){
+                //check if idx_in_prev is a valid vertex in previous frame
+                if(!std::isnan(prev_frame.N_gk[idx_in_prev][0])){
+                    // check if distances between corresponding vectors are below threshold
+                    if((curr_V_k_transformed[i] - prev_frame.V_gk[idx_in_prev]).norm() <= this->distance_threshold){
+                        // check if distances between corresponding angles are below threshold
+                        if((curr_N_k_transformed[i].dot(prev_frame.N_gk[idx_in_prev])) <= this->angle_threshold){
+                            matches.push_back(std::make_pair(i, idx_in_prev));
+                        }
+                    }
+                }
             }
         }
     }
@@ -33,13 +43,7 @@ void ICP::correspondence_finder(Eigen::Matrix4f T_curr_frame, Frame & curr_frame
 Eigen::Matrix4f ICP::point_to_plane_solver(Frame & curr_frame, Frame & prev_frame, int iterations, bool cuda){
     
     // source is the live frame F_k and the prev_frame is the ray-casted previous frame F_k-1
-
-    Eigen::Matrix4f T_gk_1 = prev_frame.T_gk;
-
-    bool prev_frame_transform_calculated = false;
-
-    Eigen::Matrix4f T_gk = Eigen::Matrix4f::Identity();
-    Eigen::Matrix4f T_gk_z;
+    Eigen::Matrix4f T_gk_z = Eigen::Matrix4f::Identity();
 
     std::vector<Eigen::Vector3f> source_vectors = curr_frame.V_k; 
 
@@ -51,7 +55,7 @@ Eigen::Matrix4f ICP::point_to_plane_solver(Frame & curr_frame, Frame & prev_fram
 
             std::vector<std::pair<int, int>> correspondences;
 
-            this->correspondence_finder(T_gk, curr_frame, prev_frame, correspondences);
+            this->correspondence_finder(T_gk_z, curr_frame, prev_frame, correspondences);
             
             int corr_size = correspondences.size();
             
@@ -59,15 +63,15 @@ Eigen::Matrix4f ICP::point_to_plane_solver(Frame & curr_frame, Frame & prev_fram
             Eigen::VectorXf b = Eigen::VectorXf::Zero(corr_size);
             
             // for(auto i: source.M_k1){
-            for(int j = 0; j < corr_size; j++){
+            for(int j = 0; j < corr_size; ++j){
                 // get each point's row in the A matrix     
                 // we have to transform our source.V_k into our estimated transformation matrix
-                Eigen::Vector3f s_i = T_gk.block(0,0,3,3) * curr_frame.V_k[correspondences[j].first] + T_gk.block(0,3,3,1);
+                Eigen::Vector3f s_i = T_gk_z.block(0,0,3,3) * curr_frame.V_k[correspondences[j].first] + T_gk_z.block(0,3,3,1);
                 Eigen::Vector3f d_i = prev_frame.V_gk[correspondences[j].second];
                 
-                Eigen::Vector3f cross_i = s_i.cross(d_i);
-
                 Eigen::Vector3f n_i = prev_frame.N_gk[correspondences[j].second];
+
+                Eigen::Vector3f cross_i = s_i.cross(n_i);
                 
                 // this is innefficient since T_gk_1 and V_k are already known and constant 
                 A(j, 0) = cross_i[0];
@@ -80,21 +84,26 @@ Eigen::Matrix4f ICP::point_to_plane_solver(Frame & curr_frame, Frame & prev_fram
                 b(j) = n_i.dot(d_i) - n_i.dot(s_i);
                 
             }
-            //could be more efficient maybe because ATA is a symmetric matrix
+            // std::cout << A;
+            // std::cout << b;
 
             Eigen::Vector<float, 6> x = A.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(b);
 
+            //could be more efficient maybe because ATA is a symmetric matrix
             // Eigen::Matrix<float,-1 , -1, Eigen::RowMajor> U = (A.transpose() * A).ldlt().matrixU(); //Upper Triangle of A -> row major according to documentation performance
             // Eigen::Vector<float, 6> y = U.triangularView<Eigen::Upper>().solve(A.transpose() * b);
             // Eigen::Vector<float, 6> x = U.triangularView<Eigen::Upper>().solve(y);
             
+            float alpha = x[0];
+            float beta = x[1];
+            float gamma = x[2];
 
-            T_gk_z <<      1 ,  x[2] , -x[1] , x[3],
-                       -x[2] ,   1   ,  x[0] , x[4],
-                        x[1] , -x[0] ,   1   , x[5],
+
+            T_gk_z <<      1 ,  alpha*beta - gamma , alpha*gamma + beta , x[3],
+                       gamma ,  alpha*beta*gamma + 1   ,  beta*gamma - alpha , x[4],
+                       -beta , alpha ,   1   , x[5],
                         0    ,  0    ,   0   ,  1  ; 
             
-            T_gk = T_gk_z * T_gk;
             
         }
         
@@ -104,9 +113,13 @@ Eigen::Matrix4f ICP::point_to_plane_solver(Frame & curr_frame, Frame & prev_fram
         //parallelization part
     }
     
-    return T_gk;
+    return T_gk_z;
     
     }
+
+Eigen::Matrix4f ICP::pyramid_ICP(Frame_Pyramid & curr_frame, Frame_Pyramid & prev_frame, bool cuda){
+    
+}
 
 int main(){
 
@@ -120,7 +133,7 @@ int main(){
     Frame_Pyramid* frame2 = new Frame_Pyramid(*FreeImage_Load(FreeImage_GetFileType(depth_map_dir_2), depth_map_dir_2));
     frame2->Depth_Pyramid[0]->save_off_format("/home/amroabuzer/Desktop/KinectFusion/KinectFusion-Cool-Edition/scene2.obj");
 
-    ICP* icp = new ICP(*frame1, *frame2, 0.001f);
+    ICP* icp = new ICP(*frame1, *frame2, 0.1f, 0.1f);
     
     
     auto T = icp->point_to_plane_solver(*frame1->Depth_Pyramid[0], *frame2->Depth_Pyramid[0], 4, false);
