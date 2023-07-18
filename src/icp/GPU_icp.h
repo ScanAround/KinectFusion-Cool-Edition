ce2c4215714796727f4a0c3a6a52e0bfba04dead
amroabuzer <amroabuzer@gmail.com>
2023-07-18 19:41:32 +0200

    added distance and angle thresholds

--- src/icp/GPU_icp.h
+++ src/icp/GPU_icp.h
@@ -8,14 +8,17 @@ class ICP{
 
 public:
 
-    ICP(const Frame_Pyramid& F_k, const Frame_Pyramid& F_k__1, const double convergence_threshold): 
+    ICP(const Frame_Pyramid& F_k, const Frame_Pyramid& F_k__1, const float distance_threshold, const float angle_threshold): 
     F_k(F_k), 
     F_k__1(F_k__1), 
-    convergence_threshold(convergence_threshold){};
+    distance_threshold(distance_threshold),
+    angle_threshold(angle_threshold){};
     
     ~ICP();
 
     Eigen::Matrix4f point_to_plane_solver(Frame & curr_frame, Frame & prev_frame, int iterations, bool cuda);
+
+    Eigen::Matrix4f pyramid_ICP(Frame_Pyramid & curr_frame, Frame_Pyramid & prev_frame, bool cuda);
 
     void correspondence_finder(Eigen::Matrix4f T_curr_frame, Frame & curr_frame, Frame & prev_frame, std::vector<std::pair<int, int>>& matches);
 
@@ -23,5 +26,6 @@ private:
 
     const Frame_Pyramid F_k;
     const Frame_Pyramid F_k__1;
-    const double convergence_threshold;
+    const float distance_threshold;
+    const float angle_threshold;
 };
