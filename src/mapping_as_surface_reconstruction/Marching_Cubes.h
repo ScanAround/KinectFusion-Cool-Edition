e875cc23d9a2170c47aee15b507d99fe97c1da63
amroabuzer <amroabuzer@gmail.com>
2023-07-20 11:58:20 +0200

    Making a voxel to mesh meme

---
+++ src/mapping_as_surface_reconstruction/Marching_Cubes.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <eigen3/Eigen/Dense>
+#include "voxel_grid.h"
+
+class Marching_Cubes{
+    public:
+        static void Mesher(kinect_fusion::VoxelGrid& grid, std::string FileName){
+            
+        };
+    private:
+};
