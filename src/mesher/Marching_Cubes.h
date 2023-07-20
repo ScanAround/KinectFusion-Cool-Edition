6bae6e963116bc71f386553ca8e8b94819385c0b
amroabuzer <amroabuzer@gmail.com>
2023-07-20 12:17:17 +0200

    Marching Cube Folder

---
+++ src/mesher/Marching_Cubes.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <eigen3/Eigen/Dense>
+#include "../mapping_as_surface_reconstruction/voxel_grid.h"
+
+class Marching_Cubes{
+    public:
+        static void Mesher(kinect_fusion::VoxelGrid& grid, std::string obj_file){
+            
+        };
+
+        static void Mesher(std::string grid_file, std::string obj_file){
+            
+        };
+    private:
+};
