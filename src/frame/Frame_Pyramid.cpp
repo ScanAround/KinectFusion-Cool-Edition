a057f9bdc4c0814c305e12e1a793171bcf0cf930
amroabuzer <amroabuzer@gmail.com>
2023-07-22 19:52:26 +0200

    added image_loc constructor for frame_pyramid

--- src/frame/Frame_Pyramid.cpp
+++ src/frame/Frame_Pyramid.cpp
@@ -25,6 +25,26 @@ Frame_Pyramid::Frame_Pyramid(FIBITMAP & dib){
     Depth_Pyramid[2] -> process_image(0.01, 1.0, 5);
 }
 
+Frame_Pyramid::Frame_Pyramid(const char * image_dir){
+
+    T_gk << Eigen::Matrix4f::Identity();
+
+    Depth_Pyramid[0] = new Frame(image_dir, T_gk);
+    Depth_Pyramid[0] -> process_image();
+
+    Depth_Pyramid[1] = new Frame(*FreeImage_Rescale(Depth_Pyramid[0] -> filtered_dib, 
+                                                    Depth_Pyramid[0] -> width/2,
+                                                    Depth_Pyramid[0] -> height/2,
+                                                    FILTER_BOX), T_gk, 2.0f);
+    Depth_Pyramid[1] -> process_image(0.01, 2.0, 10);
+
+    Depth_Pyramid[2] = new Frame(*FreeImage_Rescale(Depth_Pyramid[1] -> filtered_dib, 
+                                                    Depth_Pyramid[1] -> width/2,
+                                                    Depth_Pyramid[1] -> height/2,
+                                                    FILTER_BOX), T_gk, 4.0f);
+    Depth_Pyramid[2] -> process_image(0.01, 1.0, 5);
+}
+
 Frame_Pyramid::~Frame_Pyramid(){
     delete Depth_Pyramid[0];
     delete Depth_Pyramid[1];
