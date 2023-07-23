cd8742010a792696b7df6c4f3f32e6d880664996
amroabuzer <amroabuzer@gmail.com>
2023-07-23 19:46:07 +0200

    still working on merging everything

--- src/Kinect_Fusion.cpp
+++ src/Kinect_Fusion.cpp
@@ -9,7 +9,7 @@ int main(){
     std::unique_ptr<Marching_Cubes> mesher = std::make_unique<Marching_Cubes>();
     
     //initiating grid
-    Eigen::Vector3d gridSize(2,2,2); 
+    Eigen::Vector3d gridSize(4,4,4); 
     unsigned int res = 128;
     kinect_fusion::VoxelGrid grid(res ,res ,res ,gridSize);
     float mu = 0.02;
@@ -18,22 +18,26 @@ int main(){
     auto filenames = kinect_fusion::utility::getPngFilesInDirectory(s_dir);
     
     Frame_Pyramid curr_frame(s_dir + "/" + filenames[0]);
+    curr_frame.Depth_Pyramid[0]->save_off_format("outputs/point_clouds/G.obj");
     //somehow we're getting a problem because of our initial T_gk probably
     grid.updateGlobalTSDF(*curr_frame.Depth_Pyramid[0], mu);
     mesher -> Mesher(grid, 0, "outputs/meshes/mesh_1.off");
     auto T = curr_frame.T_gk;
 
-    for(int file_idx = 0; file_idx < filenames.size(); file_idx += 2){
+    for(int file_idx = 0; file_idx < filenames.size(); ++file_idx){
         Frame_Pyramid prev_frame(s_dir + "/" + filenames[file_idx]);
-        prev_frame.T_gk = T;
         Frame_Pyramid curr_frame(s_dir + "/" + filenames[file_idx + 1]);
-        ICP icp(prev_frame, curr_frame, 0.1f, 1.1f);
+        prev_frame.set_T_gk(T);
+        curr_frame.set_T_gk(T);
+        
+        ICP icp(curr_frame, prev_frame, 0.05f, 0.5f);
         T = icp.pyramid_ICP(false);
         
         grid.updateGlobalTSDF(*curr_frame.Depth_Pyramid[0], mu);
         
-        curr_frame.Depth_Pyramid[0]->save_off_format("outputs/point_clouds/pc" +std::to_string(file_idx % 2) + ".obj");
+        curr_frame.Depth_Pyramid[0]->save_off_format("outputs/point_clouds/pc" +std::to_string(file_idx / 2) + ".obj");
+        curr_frame.Depth_Pyramid[0]->save_G_off_format("outputs/point_clouds/pc_G" +std::to_string(file_idx / 2) + ".obj");
 
-        mesher -> Mesher(grid, 0, "outputs/meshes/mesh" + std::to_string(file_idx % 2) + ".off");
+        mesher -> Mesher(grid, 0, "outputs/meshes/mesh" + std::to_string(file_idx / 2) + ".off");
     }
 }
