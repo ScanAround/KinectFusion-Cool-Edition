fae84cc30ac132225ef83d820ff0dd982b8aea58
amroabuzer <amroabuzer@gmail.com>
2023-07-26 12:54:16 +0200

    finished general idea but now need to make lower triangle only update

--- src/gpu/icp/icp.cu
+++ src/gpu/icp/icp.cu
@@ -2,7 +2,17 @@
 #include "../../cpu/frame/Frame_Pyramid.h"
 #include <FreeImage.h>
 #include <fstream>
-#include <cusolverDn.h>
+#include <chrono>
+
+__device__
+void update_lower_triangle(float *A_jT, float* A){
+    //column major update of lower triangle 
+    for(int col = 0; col < 6; ++col){
+        for(int row = col; row < 6; ++row){
+            A[col * 6 + row] = A_jT[row] * A_jT[col];
+        }
+    }
+}
 
 //might wanna define it in header then reference here and tsdf
 __device__ 
@@ -31,9 +41,8 @@ Eigen::Vector2i vec_to_pixel(
 
 //maybe I should do shared memeory!
 __global__ 
-void A_b_finder(
-    Eigen::Matrix<float, 6, 6> *A, 
-    Eigen::Matrix<float, 6, 1> *b,
+void A_b_finder_block(
+    Eigen::Matrix<float, 21, 1> *dA_arr, Eigen::Matrix<float, 6, 1> *db_arr,
     Eigen::Matrix3f R_curr, Eigen::Vector3f t_curr, 
     Eigen::Matrix3f R_prev, Eigen::Vector3f t_prev, 
     Eigen::Vector3f *curr_V_k, Eigen::Vector3f *curr_N_k,
@@ -51,11 +60,11 @@ void A_b_finder(
             // transforming the current frame to the current pose iteration
             curr_V_k[i] = R_curr * curr_V_k[i] + t_curr;
             curr_N_k[i] = R_curr * curr_N_k[i];
-            
+
             //projecting back to previous frame pixels
             Eigen::Vector2i pixel = vec_to_pixel(curr_V_k[i], R_prev, t_prev, K, width, height);
             int idx_in_prev = pixel[1]*width + pixel[0];
-            
+
             // normals aren't calculated in last row so check if pixel is before last row
             if(idx_in_prev < (height-1) * width){
                 // checking if normals are valid in previous frame
@@ -64,101 +73,91 @@ void A_b_finder(
                     if((curr_V_k[i] - prev_V_gk[idx_in_prev]).norm() <= d_thresh){
                         // checking if angles within threshold
                         if(curr_N_k[i].dot(prev_N_gk[idx_in_prev]) >= a_thresh){
-                            
+
                             Eigen::Vector3f s_i = R_curr * curr_V_k[i] + t_curr;
                             Eigen::Vector3f d_i = prev_V_gk[idx_in_prev];
                             Eigen::Vector3f n_i = prev_N_gk[idx_in_prev];
-
-                            Eigen::Matrix<float, 6, 1> A_jT;
+                            Eigen::Matrix<float, 6, 1, Eigen::ColMajor> A_jT;
                             A_jT << s_i.cross(n_i), n_i;
-
-                            A->selfadjointView<Eigen::Lower>().rankUpdate(A_jT);
-                            *b += A_jT * (n_i.dot(d_i) - n_i.dot(s_i));
+                            
+                            Eigen::Matrix<float, 21, 1, Eigen::ColMajor> _A;
+                            // A.selfadjointView<Eigen::Lower>().rankUpdate(A_jT);
+                            update_lower_triangle(A_jT.data(), _A.data());
+                            dA_arr[id_x] += _A;
+                            db_arr[id_x] += A_jT * (n_i.dot(d_i) - n_i.dot(s_i));
+                            __syncthreads();
                         }
                     }
                 }
             }
         }
-
-}
-
+    }
 
 Eigen::Matrix4f ICP::point_to_plane_solver(Frame & curr_frame, Frame & prev_frame, int iterations, bool cuda){
     
     // source is the live frame F_k and the prev_frame is the ray-casted previous frame F_k-1
     Eigen::Matrix4f T_gk_z = curr_frame.T_gk;
+    prev_frame.apply_G_transform();
+    
+    Eigen::Vector3f *curr_V_k;
+    Eigen::Vector3f *curr_N_k;
+    Eigen::Vector3f *prev_V_gk;
+    Eigen::Vector3f *prev_N_gk;
+    
+    cudaError_t cudaStatus0 = cudaMalloc(&curr_V_k, sizeof(Eigen::Vector3f) * curr_frame.V_k.size());
+    if(cudaStatus0 != cudaSuccess){
+        std::cout << "Problem in CudaMalloc curr_V_k: " << cudaGetErrorString(cudaStatus0) << std::endl;
+    }
+    cudaStatus0 = cudaMemcpy(curr_V_k, curr_frame.V_k.data(), sizeof(Eigen::Vector3f) * curr_frame.V_k.size(), cudaMemcpyHostToDevice);
+    if(cudaStatus0 != cudaSuccess){
+        std::cout << "Problem in Cuda Copy of dA: " << cudaGetErrorString(cudaStatus0) << std::endl;
+    }
 
-    std::vector<Eigen::Vector3f> source_vectors = curr_frame.V_k; 
+    cudaStatus0 = cudaMalloc(&curr_N_k, sizeof(Eigen::Vector3f) * curr_frame.N_k.size());
+    if(cudaStatus0 != cudaSuccess){
+        std::cout << "Problem in CudaMalloc curr_N_k: " << cudaGetErrorString(cudaStatus0) << std::endl;
+    }
+    cudaStatus0 = cudaMemcpy(curr_N_k, curr_frame.N_k.data(), sizeof(Eigen::Vector3f) * curr_frame.N_k.size(), cudaMemcpyHostToDevice);
+    if(cudaStatus0 != cudaSuccess){
+        std::cout << "Problem in Cuda Copy of dA: " << cudaGetErrorString(cudaStatus0) << std::endl;
+    }
+    
+    cudaStatus0 = cudaMalloc(&prev_V_gk, sizeof(Eigen::Vector3f) * prev_frame.V_gk.size());
+    if(cudaStatus0 != cudaSuccess){
+        std::cout << "Problem in CudaMalloc prev_V_gk: " << cudaGetErrorString(cudaStatus0) << std::endl;
+    }
+    cudaStatus0 = cudaMemcpy(prev_V_gk, prev_frame.V_gk.data(), sizeof(Eigen::Vector3f) * prev_frame.V_gk.size(), cudaMemcpyHostToDevice);
+    if(cudaStatus0 != cudaSuccess){
+        std::cout << "Problem in Cuda Copy of dA: " << cudaGetErrorString(cudaStatus0) << std::endl;
+    }
+
+    cudaStatus0 = cudaMalloc(&prev_N_gk, sizeof(Eigen::Vector3f) * prev_frame.N_gk.size());
+    if(cudaStatus0 != cudaSuccess){
+        std::cout << "Problem in CudaMalloc prev_N_gk: " << cudaGetErrorString(cudaStatus0) << std::endl;
+    }
+    cudaStatus0 = cudaMemcpy(prev_N_gk, prev_frame.N_gk.data(), sizeof(Eigen::Vector3f) * prev_frame.N_gk.size(), cudaMemcpyHostToDevice);
+    if(cudaStatus0 != cudaSuccess){
+        std::cout << "Problem in Cuda Copy of dA: " << cudaGetErrorString(cudaStatus0) << std::endl;
+    }
+
     for(int i = 0; i < iterations; i++){
         
-        prev_frame.apply_G_transform();
-        Eigen::Matrix<float, 6, 6> A = Eigen::MatrixXf::Zero(6, 6); // supposed to be ATA of the system according to paper
+        Eigen::Matrix<float, 6, 6> A = Eigen::MatrixXf::Zero(6, 6);
         Eigen::Matrix<float, 6, 1> b = Eigen::MatrixXf::Zero(6, 1);
         int block_num = curr_frame.width;
         int thread_num = curr_frame.height;
 
-        Eigen::Matrix<float, 6, 6> *dA;
-        Eigen::Matrix<float, 6, 1> *db;
-        Eigen::Vector3f *curr_V_k;
-        Eigen::Vector3f *curr_N_k;
-        Eigen::Vector3f *prev_V_gk;
-        Eigen::Vector3f *prev_N_gk;
-        
-        cudaError_t cudaStatus0 = cudaMallocManaged(&dA, sizeof(Eigen::Matrix<float, 6, 6>));
-        if(cudaStatus0 != cudaSuccess){
-            std::cout << "Problem in CudaMalloc dA: " << cudaGetErrorString(cudaStatus0) << std::endl;
-        }
-        cudaStatus0 = cudaMemcpy(dA, A.data(), sizeof(Eigen::Matrix<float, 6, 6>), cudaMemcpyHostToDevice);
-        if(cudaStatus0 != cudaSuccess){
-            std::cout << "Problem in Cuda Copy of dA: " << cudaGetErrorString(cudaStatus0) << std::endl;
-        }
+        Eigen::Matrix<float, 6, 6>* dA;
+        Eigen::Matrix<float, 6, 1>* db;
+        cudaMalloc(&dA, sizeof(Eigen::Matrix<float, 6, 6>));
+        cudaMalloc(&db, sizeof(Eigen::Matrix<float, 6, 1>));
 
-        cudaStatus0 = cudaMallocManaged(&db, sizeof(Eigen::Matrix<float, 6, 1>));
-        if(cudaStatus0 != cudaSuccess){
-            std::cout << "Problem in CudaMalloc db: " << cudaGetErrorString(cudaStatus0) << std::endl;
-        }
-        cudaStatus0 = cudaMemcpy(db, b.data(), sizeof(Eigen::Matrix<float, 6, 1>), cudaMemcpyHostToDevice);
-        if(cudaStatus0 != cudaSuccess){
-            std::cout << "Problem in Cuda Copy of db: " << cudaGetErrorString(cudaStatus0) << std::endl;
-        }
-        
-        cudaStatus0 = cudaMallocManaged(&curr_V_k, sizeof(Eigen::Vector3f) * curr_frame.V_k.size());
-        if(cudaStatus0 != cudaSuccess){
-            std::cout << "Problem in CudaMalloc curr_V_k: " << cudaGetErrorString(cudaStatus0) << std::endl;
-        }
-        cudaStatus0 = cudaMemcpy(curr_V_k, curr_frame.V_k.data(), sizeof(Eigen::Vector3f) * curr_frame.V_k.size(), cudaMemcpyHostToDevice);
-        if(cudaStatus0 != cudaSuccess){
-            std::cout << "Problem in Cuda Copy of dA: " << cudaGetErrorString(cudaStatus0) << std::endl;
-        }
+        // Copy the temporary storage to the device
+        cudaMemcpy(dA, &A, sizeof(Eigen::Matrix<float, 6, 6>), cudaMemcpyHostToDevice);
+        cudaMemcpy(db, &b, sizeof(Eigen::Matrix<float, 6, 1>), cudaMemcpyHostToDevice);
 
-        cudaStatus0 = cudaMallocManaged(&curr_N_k, sizeof(Eigen::Vector3f) * curr_frame.N_k.size());
-        if(cudaStatus0 != cudaSuccess){
-            std::cout << "Problem in CudaMalloc curr_N_k: " << cudaGetErrorString(cudaStatus0) << std::endl;
-        }
-        cudaStatus0 = cudaMemcpy(curr_N_k, curr_frame.N_k.data(), sizeof(Eigen::Vector3f) * curr_frame.N_k.size(), cudaMemcpyHostToDevice);
-        if(cudaStatus0 != cudaSuccess){
-            std::cout << "Problem in Cuda Copy of dA: " << cudaGetErrorString(cudaStatus0) << std::endl;
-        }
 
-        cudaStatus0 = cudaMallocManaged(&prev_V_gk, sizeof(Eigen::Vector3f) * prev_frame.V_gk.size());
-        if(cudaStatus0 != cudaSuccess){
-            std::cout << "Problem in CudaMalloc prev_V_gk: " << cudaGetErrorString(cudaStatus0) << std::endl;
-        }
-        cudaStatus0 = cudaMemcpy(prev_V_gk, prev_frame.V_gk.data(), sizeof(Eigen::Vector3f) * prev_frame.V_gk.size(), cudaMemcpyHostToDevice);
-        if(cudaStatus0 != cudaSuccess){
-            std::cout << "Problem in Cuda Copy of dA: " << cudaGetErrorString(cudaStatus0) << std::endl;
-        }
-
-        cudaStatus0 = cudaMallocManaged(&prev_N_gk, sizeof(Eigen::Vector3f) * prev_frame.N_gk.size());
-        if(cudaStatus0 != cudaSuccess){
-            std::cout << "Problem in CudaMalloc prev_N_gk: " << cudaGetErrorString(cudaStatus0) << std::endl;
-        }
-        cudaStatus0 = cudaMemcpy(prev_N_gk, A.data(), sizeof(Eigen::Vector3f) * prev_frame.N_gk.size(), cudaMemcpyHostToDevice);
-        if(cudaStatus0 != cudaSuccess){
-            std::cout << "Problem in Cuda Copy of dA: " << cudaGetErrorString(cudaStatus0) << std::endl;
-        }
-
-        A_b_finder <<<block_num, thread_num>>>(
+        A_b_finder_block <<<block_num, thread_num>>>(
             dA, db, 
             curr_frame.T_gk.block(0,0,3,3), curr_frame.T_gk.block(0,3,3,1),
             prev_frame.T_gk.block(0,0,3,3), prev_frame.T_gk.block(0,3,3,1),
@@ -169,21 +168,24 @@ Eigen::Matrix4f ICP::point_to_plane_solver(Frame & curr_frame, Frame & prev_fram
             distance_threshold, angle_threshold
         );
         cudaDeviceSynchronize();
+        // for()
+        cudaStatus0 = cudaMemcpy(&A, dA, sizeof(Eigen::Matrix<float, 6, 6>) * block_num, cudaMemcpyDeviceToHost);
+        if(cudaStatus0 != cudaSuccess){
+            std::cout << "Problem in Cuda Copy of dA to A: " << cudaGetErrorString(cudaStatus0) << std::endl;
+        }
         
-        cudaStatus0 = cudaMemcpy(A.data(), dA, sizeof(Eigen::Matrix<float, 6, 6>), cudaMemcpyDeviceToHost);
+        cudaStatus0 = cudaMemcpy(&A, db, sizeof(Eigen::Matrix<float, 6, 1>) * block_num, cudaMemcpyDeviceToHost);
         if(cudaStatus0 != cudaSuccess){
-            std::cout << "Problem in Copying A back to host: " << cudaGetErrorString(cudaStatus0) << std::endl;
-        };
-        cudaStatus0 = cudaMemcpy(b.data(), db, sizeof(Eigen::Matrix<float, 6, 1>), cudaMemcpyDeviceToHost);
-        if(cudaStatus0 != cudaSuccess){
-            std::cout << "Problem in Copying b back to host: " << cudaGetErrorString(cudaStatus0) << std::endl;
-        };
-        cudaFree(curr_V_k);
-        cudaFree(curr_N_k);
-        cudaFree(prev_V_gk);
-        cudaFree(prev_N_gk);
+            std::cout << "Problem in Cuda Copy of db to b: " << cudaGetErrorString(cudaStatus0) << std::endl;
+        }
+        
+        cudaError_t cudaStatus = cudaGetLastError();
+        if (cudaStatus != cudaSuccess) {
+            std::cout << "CUDA error: " << cudaGetErrorString(cudaStatus) << std::endl;
+        }
 
         Eigen::Vector<float, 6> x = A.ldlt().solve(b); //ldlt because ATA not always Positive Definite
+        
         
         float alpha = x[0];
         float beta = x[1];
@@ -197,6 +199,11 @@ Eigen::Matrix4f ICP::point_to_plane_solver(Frame & curr_frame, Frame & prev_fram
         
         
     }
+
+    cudaFree(curr_V_k);
+    cudaFree(curr_N_k);
+    cudaFree(prev_V_gk);
+    cudaFree(prev_N_gk);
         
     return T_gk_z;
     
@@ -225,15 +232,18 @@ int main(){
     const char* depth_map_dir_2 = "/home/amroabuzer/Desktop/KinectFusion/KinectFusion-Cool-Edition/data/rgbd_dataset_freiburg1_xyz/depth/1305031102.194330.png";
 
     Frame_Pyramid* frame1 = new Frame_Pyramid(*FreeImage_Load(FreeImage_GetFileType(depth_map_dir_1), depth_map_dir_1));
-    frame1->Depth_Pyramid[0]->save_off_format("/home/amroabuzer/Desktop/KinectFusion/KinectFusion-Cool-Edition/scene1.obj");
+    frame1->Depth_Pyramid[0]->save_off_format("scene1.obj");
 
     Frame_Pyramid* frame2 = new Frame_Pyramid(*FreeImage_Load(FreeImage_GetFileType(depth_map_dir_2), depth_map_dir_2));
-    frame2->Depth_Pyramid[0]->save_off_format("/home/amroabuzer/Desktop/KinectFusion/KinectFusion-Cool-Edition/scene2.obj");
+    frame2->Depth_Pyramid[0]->save_off_format("scene2.obj");
 
-    ICP* icp = new ICP(*frame1, *frame2, 0.1f, 1.1f);
-
-    auto T = icp -> pyramid_ICP(false);
-
+    auto start = std::chrono::high_resolution_clock::now();
+    ICP icp(*frame1, *frame2, 0.1f, 1.1f);
+    auto T = icp.pyramid_ICP(false);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
+    std::cout << "time for execution: " << duration << std::endl; 
+    
     std::cout << T;
 
     std::vector<Eigen::Vector3f> V_tk;
