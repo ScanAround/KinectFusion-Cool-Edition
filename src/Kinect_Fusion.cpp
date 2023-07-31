a2b1187053743ce0911cc2e846897563ba3737db
amroabuzer <amroabuzer@gmail.com>
2023-07-31 19:47:26 +0200

    keepin some merger changes I made

--- src/Kinect_Fusion.cpp
+++ src/Kinect_Fusion.cpp
@@ -20,7 +20,7 @@ int main(){
     
     //initiating grid
     Eigen::Vector3d gridSize(4,4,4); 
-    unsigned int res = 256;
+    unsigned int res = 128;
     kinect_fusion::VoxelGrid grid(res ,res ,res ,gridSize, curr_frame.Depth_Pyramid[0]->center_of_mass.cast<double>());
     float mu = 0.02;
     
@@ -30,25 +30,29 @@ int main(){
     auto T = curr_frame.T_gk;
 
     for(int file_idx = 0; file_idx < filenames.size()-1; ++file_idx){
-        // Raycasting prev_r(grid, T.block(0,0,3,3), T.block(0,3,3,1));
-        // prev_r.castAllCuda();
-        // Frame_Pyramid prev_frame(prev_r.getVertices(), prev_r.getNormals(), T);
-        Frame_Pyramid prev_frame(s_dir + "/" + filenames[file_idx]);
+        Raycasting prev_r(grid, T.block(0,0,3,3), T.block(0,3,3,1));
+        prev_r.castAllCuda();
+        Frame_Pyramid prev_frame(prev_r.getVertices(), prev_r.getNormals(), T);
+        // Frame_Pyramid prev_frame(s_dir + "/" + filenames[file_idx]);
         prev_frame.set_T_gk(T);
 
         // prev_frame.Depth_Pyramid[0]->save_G_off_format("outputs/point_clouds/pc_G_previous" + std::to_string(file_idx) + ".obj");
 
         Frame_Pyramid curr_frame_(s_dir + "/" + filenames[file_idx + 1]);
         curr_frame_.set_T_gk(T); // done so converging is faster (theoretically + still testing)
+        // curr_frame_.Depth_Pyramid[0]->save_off_format("outputs/point_clouds/bic_pc" +std::to_string(file_idx) + ".obj");
+        // curr_frame_.Depth_Pyramid[0]->save_G_off_format("outputs/point_clouds/bic_pc_G" +std::to_string(file_idx) + ".obj");
         
-        ICP icp(curr_frame_, prev_frame, 0.1f, 0.7f);
+        ICP icp(curr_frame_, prev_frame, 0.04f, 0.5f);
         T = icp.pyramid_ICP(false);
 
         grid.updateGlobalTSDF(*curr_frame_.Depth_Pyramid[0], mu);
-        // curr_frame_.Depth_Pyramid[0]->save_off_format("outputs/point_clouds/pc" +std::to_string(file_idx) + ".obj");
-        // curr_frame_.Depth_Pyramid[0]->save_G_off_format("outputs/point_clouds/pc_G" +std::to_string(file_idx) + ".obj");
 
-        if(file_idx != 0 && file_idx % 5 == 0) mesher -> Mesher(grid, "outputs/meshes/mesh" + std::to_string(file_idx) + ".off");
+        if(file_idx != 0 && file_idx % 5 == 0) {
+            mesher -> Mesher(grid, "outputs/meshes/mesh" + std::to_string(file_idx) + ".off");
+            curr_frame_.Depth_Pyramid[0]->save_off_format("outputs/point_clouds/pc" +std::to_string(file_idx) + ".obj");
+            curr_frame_.Depth_Pyramid[0]->save_G_off_format("outputs/point_clouds/pc_G" +std::to_string(file_idx) + ".obj");
+        }
     }
 
     FreeImage_DeInitialise();
